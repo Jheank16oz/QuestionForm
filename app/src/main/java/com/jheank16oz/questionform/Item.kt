b17d3f6faeb82b8e2ae7666531cfcdf9ad7e71b1
jheank <jcarlos@ikatechsolutions.com>
2019-01-22 07:35:48 -0500

    added parcelable implementation

--- app/src/main/java/com/jheank16oz/questionform/Item.kt
+++ app/src/main/java/com/jheank16oz/questionform/Item.kt
@@ -1,29 +1,52 @@
 package com.jheank16oz.questionform
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-class Item {
-    @SerializedName("id")
-    @Expose
-    var id: Int = 0
-    @SerializedName("question_name")
-    @Expose
-    var tittle: String? = null
-    @SerializedName("value_catalogue")
-    @Expose
-    var catalogue: String? = null
-    @SerializedName("childs")
-    @Expose
-    var dependants: List<Int>? = null
+class Item(@SerializedName("id")
+           @Expose var id: Int, @SerializedName("question_name")
+           @Expose var tittle: String?, @SerializedName("value_catalogue")
+           @Expose var catalogue: String?, @SerializedName("childs")
+           @Expose var dependants: List<Int>?) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readInt(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.createIntArray().toList())
 
     override fun toString(): String {
         return if (tittle != null) tittle!! else ""
     }
 
-    constructor(id:Int, tittle:String ){
-        this.id = id
-        this.tittle = tittle
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(tittle)
+        parcel.writeString(catalogue)
+        parcel.writeIntList(dependants)
     }
+
+    private fun Parcel.writeIntList(input:List<Int>?) {
+        input?.size?.let { writeInt(it) } // Save number of elements.
+        input?.forEach(this::writeInt) // Save each element.
+    }
+
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Item> {
+        override fun createFromParcel(parcel: Parcel): Item {
+            return Item(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Item?> {
+            return arrayOfNulls(size)
+        }
+    }
+
 
 }
