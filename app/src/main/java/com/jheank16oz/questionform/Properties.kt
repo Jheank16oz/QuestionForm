4ad8b4da77fa5b3f75048f0636bc9c5aa729c892
jheank <jcarlos@ikatechsolutions.com>
2019-01-22 07:39:19 -0500

    Parcelable

--- app/src/main/java/com/jheank16oz/questionform/Properties.kt
+++ app/src/main/java/com/jheank16oz/questionform/Properties.kt
@@ -1,5 +1,7 @@
 package com.jheank16oz.questionform
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
@@ -9,7 +11,7 @@ import com.google.gson.annotations.SerializedName
  *
  *  <p>Properties</p>
  */
-class Properties {
+class Properties() : Parcelable {
 
     @SerializedName("placeholder")
     @Expose
@@ -52,7 +54,7 @@ class Properties {
     var count: Int = 0
     @SerializedName("options")
     @Expose
-    var items: List<Item>? = null
+    var items: ArrayList<Item>? = null
     @SerializedName("form_count")
     @Expose
     var formCount: Int = 0
@@ -67,7 +69,63 @@ class Properties {
     var formContent: String? = null
     @SerializedName("form")
     @Expose
-    var form: List<Question>? = null
+    var form: ArrayList<Question>? = null
+
+    constructor(parcel: Parcel) : this() {
+        placeholder = parcel.readString()
+        inputType = parcel.readString()
+        length = parcel.readInt()
+        min = parcel.readDouble()
+        max = parcel.readDouble()
+        minTime = parcel.readString()
+        maxTime = parcel.readString()
+        minDate = parcel.readString()
+        maxDateTime = parcel.readString()
+        minDateTime = parcel.readString()
+        maxDate = parcel.readString()
+        minSizeMb = parcel.readInt()
+        count = parcel.readInt()
+        formCount = parcel.readInt()
+        formId = parcel.readInt()
+        formTittle = parcel.readString()
+        formContent = parcel.readString()
+        form = parcel.createTypedArrayList(Question)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(placeholder)
+        parcel.writeString(inputType)
+        parcel.writeInt(length)
+        parcel.writeDouble(min)
+        parcel.writeDouble(max)
+        parcel.writeString(minTime)
+        parcel.writeString(maxTime)
+        parcel.writeString(minDate)
+        parcel.writeString(maxDateTime)
+        parcel.writeString(minDateTime)
+        parcel.writeString(maxDate)
+        parcel.writeInt(minSizeMb)
+        parcel.writeInt(count)
+        parcel.writeInt(formCount)
+        parcel.writeInt(formId)
+        parcel.writeString(formTittle)
+        parcel.writeString(formContent)
+        parcel.writeTypedList(form)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Properties> {
+        override fun createFromParcel(parcel: Parcel): Properties {
+            return Properties(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Properties?> {
+            return arrayOfNulls(size)
+        }
+    }
 
 
 }
